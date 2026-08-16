9675782fa4b3d11d34dcb6d12a7578df543949bb
Nivin <nivin0023@gmail.com>
2026-08-16 14:41:42 +0530

    Update admin.py

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -8,3 +8,7 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ("name", "roll_no", "department", "degree", "role", "is_sso_verified")
     list_filter = ("role", "is_sso_verified", "department", "degree")
     search_fields = ("name", "roll_no", "user__username")
+@admin.register(AdminAccess)
+class AdminAccessAdmin(admin.ModelAdmin):
+    list_display = ("roll_no",)
+    search_fields = ("roll_no",)
