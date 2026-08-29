ec810cc06bdf861737793ce5e1c7d68f0f935d52
Nivin <nivin0023@gmail.com>
2026-08-29 10:14:46 +0530

    Add REST framework authentication settings

--- server/config/settings.py
+++ server/config/settings.py
@@ -163,3 +163,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
