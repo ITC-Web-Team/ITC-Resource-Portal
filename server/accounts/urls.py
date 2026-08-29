1292ee41c9b5f9993a6d5b6f0c738aa52aa85db4
Nivin <nivin0023@gmail.com>
2026-08-29 13:36:51 +0530

    Add check_admin path to urlpatterns

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import (
     sso_login,
     admin_login,
@@ -13,7 +14,7 @@ urlpatterns = [
     path("login/", views.sso_login, name="sso_login"),
     path("admin-login/", views.admin_login, name="admin_login"),
     path("callback/", views.sso_callback, name="sso_callback"),
-
+    path("check-admin/", views.check_admin, name="check_admin"),
     path("profile/me/", my_profile, name="my_profile"),
     path("profile/mentor/", my_mentor, name="my_mentor"),
     path("profile/projects/", my_projects, name="my_projects"),
