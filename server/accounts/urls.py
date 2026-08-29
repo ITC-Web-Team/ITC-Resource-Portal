06f44fa7ecfadba72bdde0a4b6ff07fa5d051800
Nivin <nivin0023@gmail.com>
2026-08-29 10:16:05 +0530

    Refactor URL patterns to import views explicitly

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from . import views
+from .views import (
+    sso_login,
+    admin_login,
+    sso_callback,
+    my_profile,
+    my_mentor,
+    my_projects,
+)
 
 
 urlpatterns = [
     path("login/", views.sso_login, name="sso_login"),
     path("admin-login/", views.admin_login, name="admin_login"),
     path("callback/", views.sso_callback, name="sso_callback"),
+
+    path("profile/me/", my_profile, name="my_profile"),
+    path("profile/mentor/", my_mentor, name="my_mentor"),
+    path("profile/projects/", my_projects, name="my_projects"),
 ]
