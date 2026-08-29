6bb89c35204408cf2398bba58edb18eeee099d77
Nivin <nivin0023@gmail.com>
2026-08-29 10:14:00 +0530

    Add URLs for SSO callback and profile endpoints
    Added new URL paths for SSO callback and profile views.

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from accounts.views import sso_callback
+from accounts.views import my_profile, my_mentor, my_projects
 from projects.views import (
     ProjectListCreateView,
     MyProjectListView,
@@ -13,9 +14,13 @@ from projects.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("callback/", sso_callback, name="sso_callback"),
 
     path("api-auth/", include("rest_framework.urls")),
 
+    path("api/profile/me/", my_profile),
+    path("api/profile/mentor/", my_mentor),
+    path("api/profile/projects/", my_projects),
     # Projects
     path("api/projects/", ProjectListCreateView.as_view()),
     path("api/projects/my/", MyProjectListView.as_view()),
